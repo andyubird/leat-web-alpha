5edd67241d60b948db2d7825e0a3037d1acbbd13
Andy Fang <andyubird@users.noreply.github.com>
2021-08-31 18:34:28 +0800

    Added CitySelector.js

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h2>首頁</h2>
+      <img src="./ADEN.png" alt="ADEN"></img>
+      <h3>地方電力分析工具 網頁版 Alpha 0.1</h3>
     </div>
   );
 }
--- src/Overall/Overall.js
+++ src/Overall/Overall.js
@@ -1,7 +1,15 @@
+import CitySelector from "../util/CitySelector";
+import Box from "@material-ui/core/Box";
+
+function log(txt) {
+  console.log(txt);
+}
+
 export default function Overall() {
   return (
-    <div>
-      <h2>整體售電分析</h2>
-    </div>
+    <Box m={2}>
+      {/*<h2>整體售電分析</h2>*/}
+      <CitySelector callbackFromParent={log} />
+    </Box>
   );
 }
