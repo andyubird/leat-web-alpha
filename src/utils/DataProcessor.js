61a0685cfc8a0bfd3ac25b9413057631445312af
Andy Fang <andyubird@users.noreply.github.com>
2021-09-10 04:08:33 +0800

    Updated DataProcessor.js

--- src/utils/DataProcessor.js
+++ src/utils/DataProcessor.js
@@ -2,48 +2,53 @@ import csvData from "../data/1_city_202107.csv";
 import { readString } from "react-papaparse";
 import Numeral from "numeraljs";
 import moment from "moment";
+import dataObj from "../data/1_city.json"
 
 //#region Process Data
 
-let headers = [
-  "日期",
-  "縣市",
-  "住宅部門售電量(度)",
-  "住宅部門用電佔比(%)",
-  "服務業部門(含包燈)(度)",
-  "服務業部門(含包燈)用電佔比(%)",
-  "農林漁牧售電量(度)",
-  "農林漁牧用電佔比(%)",
-  "工業部門售電量(度)",
-  "工業部門用電佔比(%)",
-  "合計售電量(度)",
-  "縣市用電佔比(%)"
-];
+// let headers = [
+//   "日期",
+//   "縣市",
+//   "住宅部門售電量(度)",
+//   "住宅部門用電佔比(%)",
+//   "服務業部門(含包燈)(度)",
+//   "服務業部門(含包燈)用電佔比(%)",
+//   "農林漁牧售電量(度)",
+//   "農林漁牧用電佔比(%)",
+//   "工業部門售電量(度)",
+//   "工業部門用電佔比(%)",
+//   "合計售電量(度)",
+//   "縣市用電佔比(%)"
+// ];
 
-let data = readString(csvData, {
-  quotes: false, //or array of booleans
-  quoteChar: '"',
-  escapeChar: '"',
-  delimiter: "",
-  header: false,
-  newline: "",
-  skipEmptyLines: false, //or 'greedy',
-  columns: headers, //or array of strings
-  preview: null
-}).data;
+// let data = readString(csvData, {
+//   quotes: false, //or array of booleans
+//   quoteChar: '"',
+//   escapeChar: '"',
+//   delimiter: "",
+//   header: false,
+//   newline: "",
+//   skipEmptyLines: false, //or 'greedy',
+//   columns: headers, //or array of strings
+//   preview: null,
+//   download: true,
+//   dynamicTyping: true
+// }).data;
 
-data.shift();
-data.pop();
+// console.log(data);
 
-const dataObj = data.reduce((acc, e, idx) => {
-  acc.push(
-    headers.reduce((r, h, i) => {
-      r[h] = e[i];
-      return r;
-    }, {})
-  );
-  return acc;
-}, []);
+// data.shift();
+// data.pop();
+
+// const dataObj = data.reduce((acc, e, idx) => {
+//   acc.push(
+//     headers.reduce((r, h, i) => {
+//       r[h] = e[i];
+//       return r;
+//     }, {})
+//   );
+//   return acc;
+// }, []);
 
 Object.keys(dataObj).forEach(function (key) {
   dataObj[key]["日期"] = moment(
